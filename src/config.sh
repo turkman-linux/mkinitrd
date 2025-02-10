a1d8a8f03b33c6dfee8034b3c0630b26028675c3
sulincix <parduscix@yandex.ru>
2025-02-10 17:40:05 +0000

    minor fix

--- src/config.sh
+++ src/config.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 # Hook order
-hooks="base eudev modules ext4"
+hooks="base eudev modules filesystem"
 # Module list
 # * most = copy all storage and filesystem modules
 # * dep = copy all modules used by current system
 # * all = copy all modules
 # * none = do not copy any modules
 modules="most"
-
 # Filesystems
 filesystems="ext4"
