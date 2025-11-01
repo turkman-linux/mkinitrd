21d6371c3d8249a4848cb219ec6d59ca3e00bab1
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-11-01 19:48:22 +0300

    fix modules hook

--- src/scripts/modules.sh
+++ src/scripts/modules.sh
@@ -1,5 +1,14 @@
-if [ -f /etc/modules ] ; then
-    cat /etc/modules | while read module ; do
-        modprobe $module || true
-    done
-fi
+#!/bin/sh
+function init_top(){
+    if [ -f /etc/modules ] ; then
+        cat /etc/modules | while read module ; do
+            if [ "$module" == "" ] ; then
+                modprobe $module || true
+            fi
+        done
+    fi
+}
+
+function init_bottom(){
+    :
+}
