e75b0ad74dd91e7473070c3ab6264491bc0ab55e
aliriza <aliriza.keskin@pardus.org.tr>
2025-11-16 16:14:01 +0300

    remove cpu info hide because of buggy

--- src/scripts/privacy.sh
+++ src/scripts/privacy.sh
@@ -3,12 +3,6 @@ init_top(){
 }
 
 init_bottom(){
-    # generate read only empty file
-    > /dev/empty
-    mount --bind -o ro  /dev/empty /dev/empty
-    # hide cpuinfo
-    mount --bind -o ro /dev/empty /proc/cpuinfo
-    mount -t tmpfs -o ro tmpfs /sys/bus/cpu
     # hide hardware info
     mount -t tmpfs -o ro tmpfs /sys/class/dmi
     mount -t tmpfs -o ro tmpfs /sys/devices/virtual/dmi
